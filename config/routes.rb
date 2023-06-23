8f20a871fb79dd31a108c1b8f38946acd601e0bd
shota27shota <136681041+shota27shota@users.noreply.github.com>
2023-06-23 17:48:16 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
